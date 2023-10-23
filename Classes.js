64edc5e3f3df70d9e46f07c76389f04ffe6fcb44
Mohamed Taha <Taha_Acme@hotmail.com>
2023-10-23 16:30:48 +0200

    Samurai walks now

--- Classes.js
+++ Classes.js
@@ -1,3 +1,18 @@
-const game = new Game();
-const player = new Player();
-const demon = new Demon();
+
+
+
+class Game{
+    constructor(score, gameover){
+        this.score= score;
+        this.gameover= false;
+    }
+}
+
+class Player {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.speed = 10; 
+        this.element = document.getElementById("player");
+    }
+}
--- script.js
+++ script.js
@@ -5,18 +5,55 @@ document.addEventListener("DOMContentLoaded", function() {
     const startButton = document.getElementById("startButton");
     const exitButton = document.getElementById("exitButton");
     const menu = document.getElementById("menu");
-    const game = document.getElementById("game");
+    const board = document.getElementById("board");
+    
 
     startButton.addEventListener("click", function() {
         // Hide the menu and show the game content
         menu.style.display = "none";
-        game.style.display = "block";
+        board.style.display = "block";
     });
 
     exitButton.addEventListener("click", function() {
         document.body.innerHTML = "<h1>Thank you for playing!</h1>";
+        //Thank you for playing!
     });
 });
 
+const player = new Player();
+const game = new Game();
 
+//assigning board elements to variables to be the size of the client.
+const boardWidth = document.getElementById("board").clientWidth;
+const boardHeight = document.getElementById("board").clientHeight;
+const boardElement = document.getElementById("board");
+
+let  playerPositionX = 0;
+let playerPositionY = 0;
+
+let playerDirectionY = "down"
+let PlayerDirectionX = "right"
+
+let playerSpeed = 10;
+
+document.addEventListener("keydown", (event) => {
+    event.preventDefault()
+    if (event.key === "ArrowUp") {
+        player.y -= player.speed;
+        console.log("UP");
+    } else if (event.key === "ArrowDown") {
+        player.y += player.speed;
+        console.log("Down");
+    } else if (event.key === "ArrowLeft") {
+        player.x -= player.speed;
+        console.log("Left");
+    } else if (event.key === "ArrowRight") {
+        player.x += player.speed;
+        console.log("Right");
+    }
+    player.element.style.top = `${player.y}px`;
+    player.element.style.left = `${player.x}px`;
+});
+
+let animationId;
 
